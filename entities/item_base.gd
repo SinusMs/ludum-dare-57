53c19406c3e9899d171b62560e44ca562e709d54
LipskiDev <max.lipski@hotmail.com>
2025-04-05 13:55:05 +0200

    Implemented Item Base Class

---
+++ entities/item_base.gd
@@ -0,0 +1,11 @@
+class_name ItemBase
+
+extends Control
+
+@export var item_name: String = ""
+@export_multiline var description: String = ""
+@export var rarity := Utils.RARITY.COMMON
+
+func _on_gui_input(event:InputEvent) -> void:
+	if event.is_action_pressed("left_click"): 
+		Utils.currently_selected_item = self
