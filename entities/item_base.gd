235a30c01de8af5c1c82f203d7a1d5de040f9ab1
Frederik Hoheisel <frederik.hoheisel@mailbox.tu-dresden.de>
2025-04-05 22:04:45 +0200

    partikel beim Kinder spawnen

--- entities/item_base.gd
+++ entities/item_base.gd
@@ -5,16 +5,17 @@ extends Control
 @export var item_name: String = ""
 @export_multiline var description: String = ""
 @export var rarity := Utils.RARITY.COMMON
-@export var children: Dictionary[PackedScene, Vector2]
-
+@export var children : Dictionary[PackedScene, Vector2]
+var rauchwolke : CPUParticles2D = preload("res://entities/rauchwolke_cpu_particles.tscn").instantiate()
+@export var mittelpunkt : Vector2 = Vector2(0, 0)
 
 func _ready() -> void:
 	$Highlight.hide()
+	mouse_filter = Control.MOUSE_FILTER_PASS
 
 
 func _on_gui_input(event:InputEvent) -> void:
 	if event.is_action_pressed("left_click"):
-		print("maus")
 		Utils.currently_selected_item = self
 		SignalBus.item_changed.emit()
 
@@ -22,10 +23,13 @@ func _on_gui_input(event:InputEvent) -> void:
 func _spawn_children() -> void:
 	if children.size() == 0:
 		return
+	rauchwolke.position = position + mittelpunkt
+	rauchwolke.emitting = true
 	for child : PackedScene in children:
 		var child_scene : ItemBase = child.instantiate()
 		child_scene.position = position + children[child]
 		get_tree().root.add_child(child_scene)
+	get_tree().root.add_child(rauchwolke)
 
 
 func _on_mouse_entered() -> void:
