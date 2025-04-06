930e33da0d0e016eee0cae672351f419bf04779f
hanskraschewski <131150531+hanskraschewski@users.noreply.github.com>
2025-04-06 21:30:41 +0200

    buttons hide in item overview (#19)

--- globals/signalbus.gd
+++ globals/signalbus.gd
@@ -3,4 +3,5 @@ extends Node
 signal item_changed(labelText: String)
 signal item_picked_up(item : ItemBase)
 signal show_achievements()
+signal close_achievements()
 signal trash_removed(trash: CharacterBody2D)
