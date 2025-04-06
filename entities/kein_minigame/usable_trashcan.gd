d2a9c5b54bf9e15b8cb09518765b44e890218c85
Frederik Hoheisel <frederik.hoheisel@mailbox.tu-dresden.de>
2025-04-06 20:19:36 +0200

    minigame steht

---
+++ entities/kein_minigame/usable_trashcan.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Highlight.hide()
+
+
+func _on_mouse_entered() -> void:
+	$Highlight.show()
+
+
+func _on_mouse_exited() -> void:
+	$Highlight.hide()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	print(body)
+	SignalBus.trash_removed.emit(body)
--- globals/signalbus.gd
+++ globals/signalbus.gd
@@ -3,3 +3,4 @@ extends Node
 signal item_changed(labelText: String)
 signal item_picked_up()
 signal show_achievements()
+signal trash_removed(trash: CharacterBody2D)
